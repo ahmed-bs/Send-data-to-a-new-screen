464eb6054851515d33a0b34a8815871c7918d677
ahmed-bs <medamed777@gmail.com>
2021-11-29 06:39:11 +0000

    4. Create a detail screen to display information about a todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+class Todo {
+  final String title;
+  final String description;
 
+  const Todo(this.title, this.description);
+}
 void main() {
   runApp(
     MaterialApp(
@@ -87,9 +92,25 @@ class TodosScreen extends StatelessWidget {
     );
   }
 }
-class Todo {
-  final String title;
-  final String description;
 
-  const Todo(this.title, this.description);
+class DetailScreen extends StatelessWidget {
+  // In the constructor, require a Todo.
+  const DetailScreen({Key? key, required this.todo}) : super(key: key);
+
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
 }
